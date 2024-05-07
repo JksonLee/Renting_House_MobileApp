77775faf94c016e7ab34180d101ced9dee4949f0
JksonLee <130548331+JksonLee@users.noreply.github.com>
2024-05-07 23:43:42 +0800

    UI of Favored, Home and RoomDetail Page

--- app/src/main/java/com/example/lifeafterdom_assignment/RoomDetailFragment.kt
+++ app/src/main/java/com/example/lifeafterdom_assignment/RoomDetailFragment.kt
@@ -5,12 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.navigation.Navigation
+
 class RoomDetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_room_detail, container, false)
+        val view = inflater.inflate(R.layout.fragment_room_detail, container, false)
+
+        val ibtnBack : ImageButton = view.findViewById(R.id.ibtnBack)
+
+        ibtnBack.setOnClickListener{
+            val action = RoomDetailFragmentDirections.actionRoomDetailFragmentToHomeFragment()
+
+            Navigation.findNavController(view).navigate(action)
+        }
+
+        return view
     }
 }
